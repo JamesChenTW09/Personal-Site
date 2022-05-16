0832cc8990b32e839167818782e5b5aefafa851c
James Chen <js1031222@gmail.com>
2022-05-16 14:42:03 +0800

    update the rwd and sort js part

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//utilities
 const select = (ele, all = false) => {
   if (all) {
     return document.querySelectorAll(ele);
@@ -15,85 +16,130 @@ const on = (type, ele, listener, all = false) => {
     }
   }
 };
+//global variable
+const about = select(".about");
+const portfolioItem = select(".portfolioItem", true);
+const portfolioContainer = select(".portfolioLink", true);
+const portfolioArrowRight = select(".fa-arrow-right", true);
 
+//event handler
+//hidden nav icon
+on("click", ".fa-linkedin", () => {
+  window.open("https://www.linkedin.com/in/james-chen-867491216/", "_blank");
+});
+on("click", ".fa-instagram", () => {
+  window.open("https://www.instagram.com/james1562454/", "_blank");
+});
+on("click", ".fa-github", () => {
+  window.open("https://github.com/JamesChenTW09?tab=repositories", "_blank");
+});
+
+//portfolio link show up when hover
+portfolioItem[0].addEventListener("mouseenter", () => {
+  portfolioArrowRight[0].style.opacity = "1";
+  portfolioContainer[0].style.height = "55px";
+});
+portfolioItem[0].addEventListener("mouseleave", () => {
+  portfolioArrowRight[0].style.opacity = "0";
+  portfolioContainer[0].style.height = "0px";
+});
+portfolioItem[1].addEventListener("mouseenter", () => {
+  portfolioArrowRight[1].style.opacity = "1";
+  portfolioContainer[1].style.height = "55px";
+});
+portfolioItem[1].addEventListener("mouseleave", () => {
+  portfolioArrowRight[1].style.opacity = "0";
+  portfolioContainer[1].style.height = "0px";
+});
+//portfolio link event
+portfolioArrowRight[0].addEventListener("click", () => {
+  window.open("https://drinkgether.com", "_blank");
+});
+portfolioArrowRight[1].addEventListener("click", () => {
+  window.open("http://34.200.206.129:3000/", "_blank");
+});
+//about part rwd size
+window.onresize = function () {
+  if (window.innerWidth > 885) {
+    about.style.gridTemplateRows = "60px 20px 100px 20px 301px";
+  } else {
+    about.style.gridTemplateRows = "repeat(11, 50px)";
+  }
+};
+
+//nav part
+(function () {
+  const navLinks = select(".navLinks li", true);
+  const main = select(".main");
+  const resume = select(".resume");
+  const portfolio = select(".portfolio");
+  const contact = select(".contact");
+  const hiddenNav = select(".hiddenNav");
+
+  on("click", ".navBurger", function (e) {
+    hiddenNav.classList.toggle("showHiddenNav");
+    this.classList.toggle("burgerTransform");
+  });
+  const scrollEleArray = [main, about, resume, portfolio, contact];
+  for (let i = 0; i < scrollEleArray.length; i++) {
+    navLinks[i].addEventListener("click", (e) => {
+      scrollEleArray[i].scrollIntoView();
+    });
+  }
+})();
+
+//main page typing animation
 var typed = new Typed("#typed", {
-  strings: ["front-end engineer", "beer lover", "cat lover"],
+  strings: ["Front-End engineer", "Beer Lover", "Travel Addict"],
   backSpeed: 40,
   typeSpeed: 70,
   startDelay: 1000,
   backDelay: 900,
   loop: true,
 });
-//arrow style and animations
+
+//skill arrow width changing
 (function () {
-  let skillset = document.querySelector(".skillTitle").children;
-  let content = document.querySelectorAll(".skillContent p");
-  if (window.innerWidth > 850) {
-    for (let i = 0; i < skillset.length; i++) {
-      skillset[i].style.width = "80%";
-      skillset[i].style.position = "relative";
-      skillset[i].addEventListener("click", (e) => {
-        if (e.target.style.width == "100%") {
-          e.target.style.width = "80%";
-        } else {
-          for (let i = 0; i < skillset.length; i++) {
-            skillset[i].style.width = "80%";
-            content[i].style.display = "none";
-          }
-          e.target.style.width = "100%";
-          content[i].style.display = "block";
+  const allSkillTitle = select(".skillTitle div", true);
+  const skillContent = select(".skillContent p", true);
+  for (let i = 0; i < allSkillTitle.length; i++) {
+    allSkillTitle[i].style.width = "80%";
+    allSkillTitle[i].addEventListener("click", (e) => {
+      const skillTitle = e.target;
+      if (skillTitle.style.width == "100%") {
+        skillTitle.style.width = "80%";
+      } else {
+        for (let i = 0; i < allSkillTitle.length; i++) {
+          allSkillTitle[i].style.width = "80%";
+          skillContent[i].style.display = "none";
         }
-      });
-    }
-    skillset[0].style.width = "100%";
-    content[0].style.display = "block";
-  }
-})();
-
-//nav click event
-//footers.getBoundingClientRect().bottom <= window.innerHeight
-(function () {
-  const navLinks = select(".navLinks li", true);
-  const main = select(".main");
-  const about = select(".about");
-  const resume = select(".resume");
-  const portfolio = select(".portfolio");
-  const contact = select(".contact");
-
-  on("click", ".navBurger", function (e) {
-    select(".hiddenNav").classList.toggle("showHiddenNav");
-    this.classList.toggle("burgerTransform");
-  });
-
-  const eleArray = [main, about, resume, portfolio, contact];
-  for (let i = 0; i < eleArray.length; i++) {
-    navLinks[i].addEventListener("click", (e) => {
-      eleArray[i].scrollIntoView();
+        skillTitle.style.width = "100%";
+        skillContent[i].style.display = "block";
+      }
     });
   }
+  allSkillTitle[0].style.width = "100%";
+  skillContent[0].style.display = "block";
 })();
 
-// rwd skill list show
+// rwd skill transform
 (function () {
-  const skillDetail = document.querySelectorAll(".skillListRWD p");
-  const skillList = document.querySelectorAll(".skillListRWD div");
-  const about = document.querySelector(".about");
-  const skillListRWD = document.querySelector(".skillListRWD");
-
-  for (let i = 0; i < skillList.length; i++) {
-    skillList[i].addEventListener("click", (e) => {
-      if (skillDetail[i].style.display === "block") {
-        about.style.gridTemplateRows = "repeat(10, 50px) 20px";
-        skillListRWD.style.gridRow = "7/12";
-        skillDetail[i].style.display = "none";
+  const skillContent = select(".skillListRWD p", true);
+  const allSkillTitle = select(".skillListRWD div", true);
+  const skillListRWD = select(".skillListRWD");
+  for (let i = 0; i < allSkillTitle.length; i++) {
+    allSkillTitle[i].addEventListener("click", (e) => {
+      if (skillContent[i].style.height === "210px") {
+        about.style.gridTemplateRows = "repeat(11, 50px)";
+        skillListRWD.style.gridRow = "6/12";
+        skillContent[i].style.height = "0";
       } else {
-        for (let i = 0; i < skillList.length; i++) {
-          skillDetail[i].style.display = "none";
+        for (let i = 0; i < allSkillTitle.length; i++) {
+          skillContent[i].style.height = "0";
         }
-        about.style.gridTemplateRows = "repeat(14,50px)";
-        skillListRWD.style.gridRow = "7/15";
-        skillDetail[i].style.display = "block";
-        skillDetail[i].classList.toggle("skillDetailUp");
+        about.style.gridTemplateRows = "repeat(15,50px)";
+        skillListRWD.style.gridRow = "6/16";
+        skillContent[i].style.height = "210px";
       }
     });
   }
